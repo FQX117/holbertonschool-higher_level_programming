08405237acf23c89a3df7d783700ce9de04fd732
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-05-03 12:26:07 -0500

    Update 5-print_comb2.py

--- 0x00-python-hello_world/5-print_comb2.py
+++ 0x00-python-hello_world/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-n = 00
-while n <= 99:
+for n in range(0, 100):
     if n <= 98:
         print("{:02}, ".format(n), end="")
-        n = n+1
     else:
         print("{:02}".format(n))
