66ee1d268a5c5321403644d7632d1ea747bb4bb6
root <blackfox950@gmail.com>
2022-05-10 11:25:11 -0700

    update

---
+++ 0x04-python-classes/4-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+class Square:
+    ''' a square class '''
+    def __init__(self, size=0):
+        self.size = size
+    def area(self):
+        return(self.__size * self.__size)
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end='')
+                print()
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
