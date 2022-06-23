0663156b04811711346cc7a867b51c4b23e2c8d7
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-23 13:25:54 -0500

    Add files via upload

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -4,21 +4,19 @@ import MySQLdb
 
 
 def print_statevalue();
-"""comment one"""
     from sys import argv
-    data = MySQLdb.connect(host="localhost", user=argv[1], 
-    pw=argv[2], database=[3])
+    db = MySQLdb.connect(host="localhost", user=argv[1],
+                         passwd=argv[2], database=argv[3])
 
-    cursor = data.cursor()
-
-    cursor = data.cursor()
+    cursor = db.cursor()
 
     cursor.execute("SELECT * FROM states WHERE name LIKE %s", (argv[4],))
     for rows in cursor.fetchall():
         print(rows)
 
     cursor.close()
-    data.close()
+    db.close()
+
 
 if __name__=="__main__":
     print_statevalue()
