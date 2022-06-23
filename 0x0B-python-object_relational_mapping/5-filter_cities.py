cc7f01a22becc950760e07af4e4cc34da1f499e4
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-23 17:02:06 -0500

    Add files via upload

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -1,22 +1,25 @@
 #!/usr/python3
-"""a script that takes in the name of a state as an argument and lists all cities of that state"""
+"""a script that takes in the name of a state as an
+ argument and lists all cities of that state"""
+
 import MySQLdb
 
 
 def listcitys():
     from sys import argv
-    data = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-    pw=argv[2], database=argv[3])
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], database=argv[3])
 
-    cursor = data.cursor()
+    cur = db.cursor()
 
-    cursor.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id\
-         = states.id WHERE states.name LIKE %s ORDER BY cities.id", (argv[4],))
-    print(", ".join(rows[0] for rows in cursor.fetchall()))
+    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id\
+        = states.id WHERE states.name LIKE %s ORDER BY cities.id", (argv[4],))
+    print(", ".join(rows[0] for rows in cur.fetchall()))
 
-    cursor.close()
-    data.close()
+    cur.close()
+    db.close()
 
-    if __name__=="__main__":
-        main()
+    
+if __name__ == "__main__":
+     main()
     
