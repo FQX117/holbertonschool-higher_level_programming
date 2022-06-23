dca760836a78d63c6094bbc445951dd76e7e3772
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-23 12:22:09 -0500

    Add files via upload

--- 0x0B-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0B-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,7 @@ all values in the states table of hbtn_0e_0_usa
 import MySQLdb
 
 
-def print_n_state():
+def print_statevalue():
     from sys import argv
     data = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                             pw=argv[2], database=argv[3])
@@ -14,7 +14,7 @@ def print_n_state():
     cursor = data.cursor()
 
     cursor.execute("SELECT * FROM states WHERE name\
-        LIKE binary %s",[format(argv[4])])
+        LIKE binary %s", [format(argv[4])])
     for rows in cursor.fetchall():
         print(rows)
 
@@ -22,5 +22,5 @@ def print_n_state():
     data.close()
 
 
-if __name__=="__name__":
-    print_n_state()
+if __name__ == "__name__":
+    print_statevalue()
