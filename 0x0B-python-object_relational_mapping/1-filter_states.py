3850baef6b25fe949fb3e6ff3d93187ff9bae6fb
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-06-23 11:57:19 -0500

    Add files via upload

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """ a script that lists all states with a name starting with N"""
+
 import MySQLdb
 
 
 def print_nstate():
     from sys import argv
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1], pw=argv[2], database=argv[3])
+    data = MySQLdb.connect(host="localhost", port=3306, user=argv[1], pw=argv[2], database=argv[3])
 
-    cursor = db.cursor()
+    cursor = data.cursor()
 
     cursor.execute("SELECT * FROM states")
     for rows in cursor.fetchall():
@@ -15,7 +16,8 @@ def print_nstate():
             print(rows)
 
     cursor.close()
-    db.close()
+    data.close()
+
 
 if __name__=="__name__":
     print_nstate()
