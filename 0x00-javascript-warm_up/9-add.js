fc9b9112ad305419876ef45c3ced69b84e650817
FQX117 <91921154+FQX117@users.noreply.github.com>
2022-08-02 20:57:41 -0500

    Update 9-add.js

--- 0x00-javascript-warm_up/9-add.js
+++ 0x00-javascript-warm_up/9-add.js
@@ -3,13 +3,13 @@
 function add (a, b) {
     const c = Number(a) + Number(b);
     return c;
-  }
+ }
   
   const a = process.argv[2];
   const b = process.argv[3];
   
   if (isNaN(a) || isNaN(b)) {
     console.log('NaN');
-  } else {
+ } else {
     console.log(add(a, b));
   }
