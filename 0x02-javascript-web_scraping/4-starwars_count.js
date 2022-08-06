2439d15ccae9f87c33d5256a39b40bfc8a289e96
FQX117 <blackfox950@gmail.com>
2022-08-06 17:11:25 -0500

    fix star wars

--- 0x02-javascript-web_scraping/4-starwars_count.js
+++ 0x02-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,13 @@
 #!/usr/bin/node
 
 const request = require('request');
-const website = process.argv[2];
+const movieId = process.argv[2];
+const website = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 request(website, function (error, response, body) {
   if (error) {
     console.error(error);
   } else {
-    const jsonReturn = JSON.parse(body).results;
-    let wedgeApperance = 0;
-    for (const movies in jsonReturn) {
-      const characters = jsonReturn[movies].characters;
-      for (const charIndex in characters) {
-        if (characters[charIndex].includes('/18/')) {
-          wedgeApperance += 1;
-        }
-      }
-    }
-    console.log(wedgeApperance);
+    const jsonReturn = JSON.parse(body);
+    console.log(jsonReturn.title);
   }
 });
